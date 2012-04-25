105e9b16dff7888efa313f488cedf45123931780
Andrew Harvey <andrew.harvey4@gmail.com>
2012-04-25 21:14:56 +1000

    give headers to csv exports but drop row count from the last line

--- asgs-join/export-to-csv.sh
+++ asgs-join/export-to-csv.sh
@@ -2,5 +2,5 @@
 
 for dataset in 2010-house 2010-senate 2007-house 2007-senate
 do
-  psql --no-align --field-separator=',' --tuples-only -f $dataset-export.sql -o $dataset.csv
+  psql --no-align --field-separator=',' -f $dataset-export.sql | head  --lines=-1 > $dataset.csv
 done
