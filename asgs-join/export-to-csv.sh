2cba2707afdcb65b19634564764e63124264355c
Andrew Harvey <andrew.harvey4@gmail.com>
2012-08-10 22:09:33 +1000

    let it work from ../ and put results into own directory

--- asgs-join/export-to-csv.sh
+++ asgs-join/export-to-csv.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
+mkdir -p csv-exports
 for dataset in 2010-house 2010-senate 2007-house 2007-senate
 do
-  psql --no-align --field-separator=',' -f $dataset-export.sql | head  --lines=-1 > $dataset.csv
+  psql --no-align --field-separator=',' -f asgs-join/$dataset-export.sql | head  --lines=-1 > csv-exports/$dataset.csv
 done
